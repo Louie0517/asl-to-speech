ae260fda1ffc4d36582bdd7b720611f9cdc0078e
Louies <138659181+Louie0517@users.noreply.github.com>
2025-09-18 23:25:22 +0800

    Delete key.py

--- key.py
+++
@@ -1,6 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="VMtwHVIntP4wLdXKXoFs")
-project = rf.workspace("louie-fsst3").project("asl-3lsdf-6qkxy")
-version = project.version(2)
-dataset = version.download("yolov8")
-                
